a5ef05fe7b540876400354bc5b4acbd92416389b
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-09-09 14:55:21 -0400

    connected to the mysql server and sql database

---
+++ index.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+// connection info for sql database
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "12345678",
+  database: "employeeTracker_DB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  connection.end();
+});
