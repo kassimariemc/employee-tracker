589bfb9eb8bf37aba3198157b590333eca5badfa
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-09-11 09:18:45 -0400

    pseudo code layout, start function layout

--- index.js
+++ index.js
@@ -18,8 +18,120 @@ const connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  connection.end();
+  start();
 });
+
+// function which prompts user to select an action
+function start() {
+  inquirer.prompt([
+    {
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Remove employee", "Update employee role", "Update employee manager", "View all roles", "Add role", "Remove role", "View all departments", "Add department", "Remove department", "Exit"]
+    }
+  ]).then(function (answer) {
+    // based on their answer, call corresponding function
+    switch (answer.action) {
+      case "View all employees":
+        queryAllEmployees();
+        break;
+      case "View all employees by department":
+        queryAllEmployeesByDept();
+        break;
+      case "View all employees by manager":
+        queryAllEmployeesByMgr();
+        break;
+      case "Add employee":
+        addEmployee();
+        break;
+      case "Remove employee":
+        removeEmployee();
+        break;
+      case "Update employee role":
+        updateEmployeeRole();
+        break;
+      case "Update employee manager":
+        updateEmployeeMgr();
+        break;
+      case "View all roles":
+        queryAllRoles();
+        break;
+      case "Add role":
+        addRole();
+        break;
+      case "Remove role":
+        removeRole();
+        break;
+      case "View all departments":
+        queryAllDepartments();
+        break;
+      case "Add department":
+        addDepartment();
+        break;
+      case "Remove department":
+        removeDepartment();
+        break;
+      case "Exit":
+        connection.end();
+        break;
+      default:
+        connection.end();
+    }
+  });
+}
+
+function queryAllEmployees() {
+
+}
+
+function queryAllEmployeesByDept() {
+
+}
+
+function queryAllEmployeesByMgr() {
+
+}
+
+function addEmployee() {
+
+}
+
+function removeEmployee() {
+
+}
+
+function updateEmployeeRole() {
+
+}
+
+function updateEmployeeMgr() {
+
+}
+
+function queryAllRoles() {
+
+}
+
+function addRole() {
+
+}
+
+function removeRole() {
+
+}
+
+function queryAllDepartments() {
+
+}
+
+function addDepartment() {
+
+}
+
+function removeDepartment() {
+
+}
